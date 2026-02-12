03135ab296afa4dd0dad3911bec3e15614ccb39e
AlfonsoMonfort <alfonso.monfort@balearia.com>
2026-02-12 13:52:16 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,40 @@
-const CACHE_NAME = "inventario-offline-v5";
+const CACHE_NAME = "inventario-cache-v1";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/app.js",
-  "/quagga.min.js",
-  "/xlsx.full.min.js"
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json",
+  "./quagga.min.js",
+  "./xlsx.full.min.js",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", function(event) {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
