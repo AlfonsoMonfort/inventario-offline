d7dc5a39af55cc7761ea4e456640a7280b6aeb33
AlfonsoMonfort <alfonso.monfort@balearia.com>
2026-02-17 10:13:51 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -158,7 +158,7 @@ function iniciarScanner() {
             top: "26.5%",
             right: "7.5%",
             left: "7.5%",
-            bottom: "26.5%"
+            bottom: "28.5%"
         }
         },
         decoder: {
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "inventario-cache-v142";
+const CACHE_NAME = "inventario-cache-v143";
 
 const urlsToCache = [
   "./",
