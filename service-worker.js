b9d45578bc08f2651ada42ac657fee040e6c797a
AlfonsoMonfort <alfonso.monfort@balearia.com>
2026-03-05 18:20:04 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,52 +1,13 @@
-const CACHE_NAME = "inventario-cache-v2";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./app.js",
-  "./manifest.json",
-  "./xlsx.full.min.js",
-  "./jspdf.umd.min.js",
-  "./JsBarcode.all.min.js",
-  "./icon-192.png",
-  "./icon-512.png"
-];
+const CACHE_NAME = "inventario-lite";
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(async cache => {
-      for (const url of urlsToCache) {
-        try {
-          await cache.add(url);
-          console.log("Cache OK:", url);
-        } catch (e) {
-          console.log("Cache ERROR:", url);
-        }
-      }
-    })
-  );
-
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
