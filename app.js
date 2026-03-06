87b8fb189ab3caf63b4cea944a09cebacfe0c58f
AlfonsoMonfort <alfonso.monfort@balearia.com>
2026-03-06 09:31:17 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1230,7 +1230,7 @@ function generarPDFEtiquetasSeleccionadas() {
     JsBarcode(canvas, codigo, {
       format: formato,
       displayValue: false,
-      width: 1,
+      width: 1.7,
       height: 40,
       margin: 0
     });
